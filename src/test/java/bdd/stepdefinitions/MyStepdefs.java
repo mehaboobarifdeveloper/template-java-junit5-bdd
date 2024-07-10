fb45535d84d54b0c3c394c89b807719952aeccc1
mehaboobarifdeveloper <mehaboobarifdeveloper@gmail.com>
2024-07-10 11:13:36 +0530

    log4j update

--- src/test/java/bdd/stepdefinitions/MyStepdefs.java
+++ src/test/java/bdd/stepdefinitions/MyStepdefs.java
@@ -3,16 +3,23 @@ package bdd.stepdefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
 
 public class MyStepdefs {
+
+    private static final Logger logger = LogManager.getLogger(MyStepdefs.class);
+
     @Given("this works as intended")
     public void thisWorksAsIntended() {
+        logger.info("Starting precondition step");
         Assertions.fail("Failure in this area-0");
     }
 
     @When("the test runner is called")
     public void theTestRunnerIsCalled() {
+        logger.info("Starting precondition step-2");
         Assertions.assertTrue(true, "Failure in this area-1");
     }
 
